80f5661057c80b53ed8e26a7950c056b4e947638
xogost <xogost@linux-5aq4.site>
2014-03-20 23:47:57 -0500

    Se agrega menu de encuestas, login, logout, almacenamiento de encuesta

--- platforms/android/assets/www/app/js/app.js
+++ platforms/android/assets/www/app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
+  'ngCookies',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
@@ -15,9 +16,9 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginController'});
-  $routeProvider.when('/instrumentos', {templateUrl: 'partials/instrumentosList.html', controller: 'instrumentosController'});
+  $routeProvider.when('/logout', {templateUrl: 'partials/logout.html', controller: 'logoutController'});
+  $routeProvider.when('/menu', {templateUrl: 'partials/menu.html', controller: 'MenuController'});
   $routeProvider.when('/instrumentouno', {templateUrl: 'partials/instrumentouno.html', controller: 'instrumentounoController'});
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  
   $routeProvider.otherwise({redirectTo: '/login'});
 }]);
--- platforms/android/assets/www/app/js/controllers.js
+++ platforms/android/assets/www/app/js/controllers.js
@@ -3,20 +3,47 @@
 /* Controllers */
 
 var ModuleMyApp = angular.module('myApp.controllers', []);
-  ModuleMyApp.controller('MyCtrl1', [function() {
+
+  //=================================================================
+  //=============        InstrumentoUno Controller   ================
+  //=================================================================
+  ModuleMyApp.controller('instrumentounoController', ['$scope', '$cookies' , function($scope, $cookies) {
+  	
+    $scope.formData = {};
+
+    $scope.guardar = function(){
+      console.log(this.formData);
+
+      var db = window.openDatabase("fedesaludDB", "1.0", "Fedesalud DB", 200000);
+      db.transaction($scope.querySaveEncuesta, $scope.errorDB);
+    };
+
+    //En caso de error
+    $scope.querySaveEncuesta = function(tx) {
+      var arrayData =  $scope.formData;
+      console.log($cookies.iduser);
+      for(var item in arrayData){
+        var sql = "INSERT INTO ENCUESTAS (IDCAMPO, VALOR, SINCRONIZADO, USUARIO) VALUES ('" + item + "','" + arrayData[item] + "', 0, '" + $cookies.user + "')";
+        //console.log(sql);
+        tx.executeSql(sql);
+      }
+      alert("La encuesta se almaceno correctamente!");
+      window.location = '#/menu'
+    };
+
+    //En caso de error
+    $scope.errorDB = function(error) {
+      alert("Error BD: " + error.code);
+      console.log(error);
+    };
 
   }]);
-  ModuleMyApp.controller('MyCtrl2', [function() {
 
-  }]);
-  ModuleMyApp.controller('instrumentosController', ['$scope', function($scope) {
-  	$scope.title = "Listado de Instrumentos";
-  }]);
-  ModuleMyApp.controller('instrumentounoController', ['$scope', function($scope) {
-  	$scope.title = "Instrumento No. 1";
-  }]);
+  //=================================================================
+  //=============        Login Contoller             ================
+  //=================================================================
 
-  ModuleMyApp.controller('loginController', ['$scope', function($scope) {
+  ModuleMyApp.controller('loginController', ['$scope', '$cookies', function($scope, $cookies) {
       
     $scope.userName = 0;
     $scope.passWord = 0; 
@@ -29,6 +56,8 @@ var ModuleMyApp = angular.module('myApp.controllers', []);
     
     $scope.queryAdmin = function (tx) {
       tx.executeSql("CREATE TABLE IF NOT EXISTS USUARIOS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE TEXT NOT NULL, PASSWORD TEXT NOT NULL)");
+      //tx.executeSql("DROP TABLE ENCUESTAS");
+      tx.executeSql("CREATE TABLE IF NOT EXISTS ENCUESTAS (ID INTEGER PRIMARY KEY AUTOINCREMENT, IDCAMPO TEXT NOT NULL, VALOR TEXT NOT NULL, SINCRONIZADO INTEGER NOT NULL, USUARIO TEXT NOT NULL)");
       var query = "SELECT * FROM USUARIOS WHERE USUARIOS.NOMBRE = 'ADMIN'";
       tx.executeSql(query, [], $scope.CreateAmdin, $scope.errorDB);
     };
@@ -39,8 +68,12 @@ var ModuleMyApp = angular.module('myApp.controllers', []);
     };
 
     $scope.onDeviceReady = function(){
-      var db = window.openDatabase("fedesaludDB", "1.0", "Fedesalud DB", 200000);
-      db.transaction($scope.queryAdmin, $scope.errorDB);
+      if($cookies.user == undefined){
+        var db = window.openDatabase("fedesaludDB", "1.0", "Fedesalud DB", 200000);
+        db.transaction($scope.queryAdmin, $scope.errorDB);
+      }else{
+        window.location = '#/menu';
+      }
     };
 
     //En caso de error
@@ -72,6 +105,7 @@ var ModuleMyApp = angular.module('myApp.controllers', []);
       var encontroUsuario = resultados.rows.length;
 
       if(encontroUsuario >= 1){
+        $scope.idUser = resultados.rows.item(0).ID;
         $scope.userName = resultados.rows.item(0).NOMBRE;
         $scope.passWord = resultados.rows.item(0).PASSWORD;
 
@@ -79,7 +113,11 @@ var ModuleMyApp = angular.module('myApp.controllers', []);
         var domPassword = document.getElementById("txtpassword");
 
         if($scope.userName == domUserName.value && $scope.passWord == domPassword.value){
-          alert("Inició Sesión! Felicitaciones");
+          var favoriteCookie = $cookies.myFavorite;
+        
+          $cookies.user = $scope.idUser + ":" + $scope.userName;
+          
+          window.location = '#/menu';
         }
         else{
           alert("Nombre de usuario o contraseña invalidos, intentelo de nuevo."); 
@@ -92,4 +130,25 @@ var ModuleMyApp = angular.module('myApp.controllers', []);
 
     document.addEventListener("deviceready", $scope.onDeviceReady , false);
 
+  }]);
+  
+  //=================================================================
+  //=============            Menu Controller         ================
+  //=================================================================
+
+  ModuleMyApp.controller('MenuController', ['$scope', '$cookies', function($scope, $cookies) {
+    if($cookies.user == undefined){
+      window.location = '#/login';
+    }
+  }]);
+
+  //=================================================================
+  //=============            Logout Controller         ================
+  //=================================================================
+
+  ModuleMyApp.controller('logoutController', ['$scope', '$cookies', function($scope, $cookies) {
+    $scope.logout = function(){
+      $cookies.user = undefined;
+      window.location = "#/login";
+    };
   }]);
