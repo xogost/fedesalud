df40c7f2927646b064e04abd42257517d3b591c6
Juan Camilo <juan@desaLinux.(none)>
2014-04-04 13:05:44 -0500

    Se realizan ajustes de sincronización ademas de ajustes en la actualización de datos

--- platforms/android/assets/www/app/js/syncController.js
+++ platforms/android/assets/www/app/js/syncController.js
@@ -21,10 +21,13 @@ ModuleMyApp.controller('sincronizarController', ['$scope', '$http' , function($s
     };
 
     $scope.pushResultServer = function(tx, result){
+      if(result.rows.length == 0){
+        jQuery("#sincronizando").html("No existen elementos a sincronizar");
+      }
       for (var i = result.rows.length - 1; i >= 0; i--) {
         $http({
             method: "GET",
-            url: "http://181.135.188.48:10005/fedesalud-sync/fedesalud-sync/index.php/getData",          
+            url: "http://198.38.92.149:8080/fedesalud-sync/index.php/getData",          
             params: {"data": JSON.stringify(result.rows.item(i))},
           }).success(function(idSync) {
             
@@ -49,7 +52,7 @@ ModuleMyApp.controller('sincronizarController', ['$scope', '$http' , function($s
       for (var i = result.rows.length - 1; i >= 0; i--) {
         $http({
             method: "GET",
-            url: "http://181.135.188.48:10005/fedesalud-sync/index.php/getData/DataEncuestas",          
+            url: "http://198.38.92.149:8080/fedesalud-sync/index.php/getData/DataEncuestas",          
             params: {"data": JSON.stringify(result.rows.item(i))},
           }).success(function(idSync) {
             
